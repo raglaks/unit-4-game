072f9dc963ae0583fdce3062dbd059cc0044164d
cholo5ntrol <raglaks94@gmail.com>
2018-08-27 19:33:47 -0500

    finished jquery code; finished bootstrap design

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,41 +10,129 @@ var crys2 = null;
 var crys3 = null;
 var crys4 = null;
 
-var crystal = null;
-
 $(document).ready(function () {
-    crys1 = Math.floor((Math.random() * 12) + 1);
 
-    crys2 = Math.floor((Math.random() * 12) + 1);
+    crys1 = parseInt(Math.floor((Math.random() * 12) + 1));
 
-    crys3 = Math.floor((Math.random() * 12) + 1);
+    crys2 = parseInt(Math.floor((Math.random() * 12) + 1));
 
-    crys4 = Math.floor((Math.random() * 12) + 1);
+    crys3 = parseInt(Math.floor((Math.random() * 12) + 1));
 
-    targetScore = Math.floor((Math.random() * 120) + 19);
+    crys4 = parseInt(Math.floor((Math.random() * 12) + 1));
 
-    $("#targetScore").append(targetScore);
+    targetScore = parseInt(Math.floor((Math.random() * 120) + 19));
+
+    $("#targetScore").html(targetScore);
+
+    function second() {
+        if (wins || losses) {
+            crys1 = parseInt(Math.floor((Math.random() * 12) + 1));
+
+            crys2 = parseInt(Math.floor((Math.random() * 12) + 1));
+
+            crys3 = parseInt(Math.floor((Math.random() * 12) + 1));
+
+            crys4 = parseInt(Math.floor((Math.random() * 12) + 1));
+
+            targetScore = parseInt(Math.floor((Math.random() * 120) + 19));
+
+            $("#targetScore").html(targetScore);
+        }
+    }
+
+    function reset() {
+        crys1 = null;
+        crys2 = null;
+        crys3 = null;
+        crys4 = null;
+        targetScore = null;
+        playerScore = null;
+        $("#targetScore").html(targetScore);
+        $("#playerScore").html(playerScore);
+    }
 
     $("#1").on("click", function () {
-        console.log("first crystal");
-        console.log(crys1);
-        console.log(crystal);
-        $("#playerScore").append(parseInt(crys1 + crystal));
+        console.log("first crystal is worth " + crys1);
+        playerScore = crys1 + playerScore;
+        console.log("your score is " + playerScore);
+        console.log("target score is " + targetScore);
+        $("#playerScore").html(playerScore);
+        if (playerScore == targetScore) {
+            console.log("you win");
+            wins++;
+            $("#wins").html(wins);
+            reset();
+            second();
+        } else if (playerScore > targetScore) {
+            console.log("you lose");
+            losses++;
+            $("#losses").html(losses);
+            reset();
+            second();
+        }
     })
 
     $("#2").on("click", function () {
-        console.log("second crystal");
-        console.log(crys2);
-        $("#playerScore").append(parseInt(crys2));
+        console.log("second crystal is worth " + crys2);
+        playerScore = crys2 + playerScore;
+        console.log("your score is " + playerScore);
+        console.log("target score is " + targetScore);
+        $("#playerScore").html(playerScore);
+        if (playerScore == targetScore) {
+            console.log("you win");
+            wins++;
+            $("#wins").html(wins);
+            reset();
+            second();
+        } else if (playerScore > targetScore) {
+            console.log("you lose");
+            losses++;
+            $("#losses").html(losses);
+            reset();
+            second();
+        }
     })
 
     $("#3").on("click", function () {
-        console.log("third crystal");
-        console.log(crys3);
+        console.log("third crystal is worth " + crys3);
+        playerScore = crys3 + playerScore;
+        console.log("your score is " + playerScore);
+        console.log("target score is " + targetScore);
+        $("#playerScore").html(playerScore);
+        if (playerScore == targetScore) {
+            console.log("you win");
+            wins++;
+            $("#wins").html(wins);
+            reset();
+            second();
+        } else if (playerScore > targetScore) {
+            console.log("you lose");
+            losses++;
+            $("#losses").html(losses);
+            reset();
+            second();
+        }
     })
 
     $("#4").on("click", function () {
-        console.log("fourth crystal");
-        console.log(crys4);
+        console.log("fourth crystal is worth " + crys4);
+        playerScore = crys4 + playerScore;
+        console.log("your score is " + playerScore);
+        console.log("target score is " + targetScore);
+        $("#playerScore").html(playerScore);
+        if (playerScore == targetScore) {
+            console.log("you win");
+            wins++;
+            $("#wins").html(wins);
+            reset();
+            second();
+        } else if (playerScore > targetScore) {
+            console.log("you lose");
+            losses++;
+            $("#losses").html(losses);
+            reset();
+            second();
+        }
     })
+
 });
